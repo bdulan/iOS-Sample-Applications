bb1707ad7fb71d4a2426c0f791709dbfb6388390
Brett Dulan <brett.dulan@redcross.org>
2015-02-06 13:17:51 -0500

    New

---
+++ MyPlaygroundNEW.playground/section-1.swift
@@ -0,0 +1,27 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+var dividend = 9
+var quotient = 0
+var divisor = 2
+var d_count = 0
+
+while (d_count < dividend)
+{
+   d_count += divisor
+    quotient++
+
+}
+
+if (dcount > divisor)
+{
+    quotient--
+}
+
+println(quotient)
+
+
+
---
+++ MyPlaygroundX.playground/section-1.swift
@@ -0,0 +1,10 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+var x = 5;
+var y = 2;
+
+var z = x/y;
+
+println(z)
